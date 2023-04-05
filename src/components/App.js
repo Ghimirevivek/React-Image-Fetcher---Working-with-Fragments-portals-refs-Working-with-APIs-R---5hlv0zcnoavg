be41d048aa62be50de8a222ecbcd08690474d41a
Ghimirevivek <stephengmre@gmail.com>
2023-04-05 19:49:12 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
+import React, { useState } from 'react'
+import '../styles/App.css'
+import { Loader } from './Loader'
+import { PhotoFrame } from './PhotoFrame'
+
 const App = () => {
-  
+  const [photodata, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [id, setId] = useState('')
+
+  const handleChange = (e) => {
+    setId(e.target.value)
+  }
+  const fetchapi = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/photos/${id}`
+      )
+      const data = await res.json()
+      setData(data)
+    } catch (err) {
+      console.log(err)
+      setData(null)
+    } finally {
+      setLoading(false)
+    }
+  }
+  const handleClick = () => {
+    fetchapi()
+  }
+  return (
+    <>
+      <div>
+        <label htmlFor="input">Enter number between 1-5000</label>
+        <input
+          type="number"
+          id="input"
+          value={id}
+          min={1}
+          max={5000}
+          onChange={handleChange}
+          required
+        />
+        <button onClick={handleClick}>Fetch</button>
+      </div>
+      {loading && <Loader />}
+      {photodata && <PhotoFrame url={photodata.url} title={photodata.title} />}
+    </>
+  )
 }
 
-
-export default App;
+export default App
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = ({ url, title }) => {
+  return (
+    <>
+      <div className="photoframe">
+        <img src={url} alt={title} />
+        <div className="caption">{title}</div>
+      </div>
+    </>
+  )
 }
